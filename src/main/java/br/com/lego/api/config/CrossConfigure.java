533112f333d72b331ae134906cfc1aa1e8d1e204
Filipe Santiago <porto.santiago@hotmail.com>
2018-11-28 09:39:48 -0300

    Add cross config

--- src/main/java/br/com/lego/api/config/CrossConfigure.java
+++ src/main/java/br/com/lego/api/config/CrossConfigure.java
@@ -1,19 +1,16 @@
 package br.com.lego.api.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-public class CrossConfigure {
+@Configuration
+@EnableWebMvc
+public class CrossConfigure implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
